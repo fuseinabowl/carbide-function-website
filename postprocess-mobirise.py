5db5961e74509a44d7f6e7ad6f6d6113cc366899
Anthony Diggle <fuseinabowl@gmail.com>
2024-01-17 17:14:03 +0000

    Make background match the articles

--- postprocess-mobirise.py
+++ postprocess-mobirise.py
@@ -2,15 +2,18 @@ from typing import List
 from pathlib import Path
 from bs4 import BeautifulSoup
 import os
+import tempfile
+import shutil
 
 script_path = Path(__file__)
 exported_website_path = script_path.parent
+css_path = exported_website_path / r'assets/theme/css/style.css'
 
-def main():
+def main() -> None:
   remove_mobirise_footers_from_pages()
   add_background_to_css()
 
-def remove_mobirise_footers_from_pages():
+def remove_mobirise_footers_from_pages() -> None:
   html_files = find_html_files_in_directory(exported_website_path)
   for file_path in html_files:
     remove_mobirise_footer(file_path)
@@ -33,18 +36,26 @@ def remove_mobirise_footer(file_path : Path) -> None:
     print(f'section: {(section.attrs["class"])}')
     if 'display-7' in section.attrs["class"]:
       section.extract()
-  #for element in tree.iter():
-  #  if element.name == 'body':
-  #    for body_element in element.iter():
-  #      classes = body_element.get('class')
-  #      if classes.contains('display-7'):
-  #        element.remove(body_element)
 
   with open(file_path, 'w') as file_handle:
     file_handle.write(str(soup))
 
-def add_background_to_css():
-  pass
+def add_background_to_css() -> None:
+  found_body_tag = False
+  with open(css_path, 'r') as source_file:
+    (destination_file_os_handle, temp_path) = tempfile.mkstemp()
+    with os.fdopen(destination_file_os_handle, 'w') as destination_file:
+      for line in source_file:
+        destination_file.write(line)
+        if line.startswith('body {'):
+          found_body_tag = True
+          destination_file.write('  background-color: #385493;\n')
+  assert(found_body_tag, 'did not find body tag in CSS file')
+  move_temp_file_to_css(temp_path, css_path)
+
+
+def move_temp_file_to_css(temp_css_file : Path, css_path : Path) -> None:
+  shutil.move(str(temp_css_file), str(css_path))
 
 if __name__ == '__main__':
   main()
